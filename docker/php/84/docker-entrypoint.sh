dac300f6dff1fc050383dcbedadd3953b8e9428d
Larry Garfield <larry@garfieldtech.com>
2024-10-04 14:43:11 -0500

    More fixes.

--- docker/php/84/docker-entrypoint.sh
+++ docker/php/84/docker-entrypoint.sh
@@ -4,7 +4,7 @@
 set -e
 
 #=== Set folder permissions ===
-folders="content/core/config content/core/data content/core/logs content/user/attachments content/user/images"
+folders="cache cache/config cache/latte cache/routes"
 
 mkdir -vp $folders
 
@@ -20,35 +20,35 @@ mkdir -vp $folders
 
 ##=== Check database vars ===
 #=== DB host ===
-if [ -z "$PMF_DB_HOST" -a ! -e "./content/core/config/database.php" ]; then
-  echo >&2 'WARN: missing PMF_DB_HOST environment variable'
-  echo >&2 '  Did you forget to --link some_mysql_container:db ?'
-else
-  #=== DB user and pass ===
-  : ${PMF_DB_USER:=root}
-  if [ "$PMF_DB_USER" = 'root' ]; then
-    : ${PMF_DB_PASS:=$DB_ENV_MYSQL_ROOT_PASSWORD}
-  fi
-
-  if [ -z "$PMF_DB_PASS" ]; then
-    echo >&2 'ERROR: missing required PMF_DB_PASS environment variable'
-    echo >&2 '  Did you forget to -e PMF_DB_PASS=... ?'
-    echo >&2
-    echo >&2 '  (Also of interest might be PMF_DB_USER and PMF_DB_NAME.)'
-    exit 1
-  #=== Setup database if needed ===
-  elif [ 0 -eq 1 ]; then # TODO : Add something like: php setup/maintenance.php --vars...
-    {
-      echo "<?php"
-      echo "\$DB['server'] = '$PMF_DB_HOST';"
-      echo "\$DB['user'] = '$PMF_DB_USER';"
-      echo "\$DB['password'] = '$PMF_DB_PASS';"
-      echo "\$DB['db'] = '${PMF_DB_NAME:-phpmyfaq}';"
-      echo "\$DB['prefix'] = '${PMF_DB_PREFIX}';"
-      echo "\$DB['type'] = '${PMF_DB_TYPE:-mysqli}';"
-    } | tee ./config/database.php
-  fi
-fi
+#if [ -z "$PMF_DB_HOST" -a ! -e "./content/core/config/database.php" ]; then
+#  echo >&2 'WARN: missing PMF_DB_HOST environment variable'
+#  echo >&2 '  Did you forget to --link some_mysql_container:db ?'
+#else
+#  #=== DB user and pass ===
+#  : ${PMF_DB_USER:=root}
+#  if [ "$PMF_DB_USER" = 'root' ]; then
+#    : ${PMF_DB_PASS:=$DB_ENV_MYSQL_ROOT_PASSWORD}
+#  fi
+#
+#  if [ -z "$PMF_DB_PASS" ]; then
+#    echo >&2 'ERROR: missing required PMF_DB_PASS environment variable'
+#    echo >&2 '  Did you forget to -e PMF_DB_PASS=... ?'
+#    echo >&2
+#    echo >&2 '  (Also of interest might be PMF_DB_USER and PMF_DB_NAME.)'
+#    exit 1
+#  #=== Setup database if needed ===
+#  elif [ 0 -eq 1 ]; then # TODO : Add something like: php setup/maintenance.php --vars...
+#    {
+#      echo "<?php"
+#      echo "\$DB['server'] = '$PMF_DB_HOST';"
+#      echo "\$DB['user'] = '$PMF_DB_USER';"
+#      echo "\$DB['password'] = '$PMF_DB_PASS';"
+#      echo "\$DB['db'] = '${PMF_DB_NAME:-phpmyfaq}';"
+#      echo "\$DB['prefix'] = '${PMF_DB_PREFIX}';"
+#      echo "\$DB['type'] = '${PMF_DB_TYPE:-mysqli}';"
+#    } | tee ./config/database.php
+#  fi
+#fi
 
 if [ -f "$APACHE_ENVVARS" ]; then
   #=== Enable htaccess for search engine optimisations ===
@@ -74,8 +74,8 @@ fi
   echo "safe_mode = Off"
   echo "log_errors = $PHP_LOG_ERRORS"
   echo "error_reporting = $PHP_ERROR_REPORTING"
-  echo "date.timezone = $PMF_TIMEZONE"
-  echo "memory_limit = $PMF_MEMORY_LIMIT"
+#  echo "date.timezone = $PMF_TIMEZONE"
+#  echo "memory_limit = $PMF_MEMORY_LIMIT"
   echo "file_upload = $PMF_ENABLE_UPLOADS"
   echo "post_max_size = $PHP_POST_MAX_SIZE"
   echo "upload_max_filesize = $PHP_UPLOAD_MAX_FILESIZE"
