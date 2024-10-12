2b98965d64eddc717fbcd37447b5ec8375d90eab
Larry Garfield <larry@garfieldtech.com>
2024-10-11 20:32:02 -0500

    Refactor the cleanup script to be more robust.

--- src/Commands/CleanGeneratedFiles.php
+++ src/Commands/CleanGeneratedFiles.php
@@ -20,63 +20,37 @@ readonly class CleanGeneratedFiles
 
     public function run(): void
     {
-        $this->clearCache();
-        $this->clearPregenerated();
+        print "Removing cache files.\n";
+        $this->rmdirContents($this->cachePath);
 
-        $this->clearEmptyDirectories($this->cachePath);
-        $this->clearEmptyDirectories($this->publicPath);
+        print "Removing generated static files.\n";
+        $this->rmdirContents($this->publicPath, fn (\SplFileInfo $f) => $f->getRealPath() !== $this->publicPath . '/index.php');
     }
 
     /**
-     * This is a fugly hack, because PHP's filesystem API is a fugly hack.
+     * Recursively removes all files in a directory, but not the directory itself.
      *
-     * This will likely fail with multiple nested directories.
-     *
-     * @todo Fix this mess.
-     *
-     * @param string $directory
-     *   The directory in which to remove empty directories.
-     * @return void
+     * @param string $dir
+     *   The directory to dlear.
+     * @param \Closure|null $filter
+     *   If specified, only files/directories that evaulate to true when passed to this
+     *   callback will be removed.
      */
-    private function clearEmptyDirectories(string $directory): void
-    {
-        $iterator = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($directory));
-        $dirs = new \CallbackFilterIterator($iterator, static fn (\SplFileInfo $f) => $f->isDir());
-        $dirs = new \CallbackFilterIterator($dirs, static fn(\SplFileInfo $f) => $f->getFilename() !== '..');
-        $dirs = new \CallbackFilterIterator($dirs, static fn(\SplFileInfo $f) => $f->getRealPath() !== $directory);
-
-        /** @var \SplFileInfo $dir */
-        foreach ($dirs as $dir) {
-            rmdir($dir->getRealPath());
+    function rmdirContents(string $dir, ?\Closure $filter = null): void {
+        $it = new \RecursiveDirectoryIterator($dir, \RecursiveDirectoryIterator::SKIP_DOTS);
+        $files = new \RecursiveIteratorIterator($it,
+            \RecursiveIteratorIterator::CHILD_FIRST);
+        if ($filter) {
+            $files = new \CallbackFilterIterator($files, $filter);
         }
-    }
-
-    private function clearCache(): void
-    {
-        print "Removing cache files.\n";
-
-        $flags = \FilesystemIterator::KEY_AS_PATHNAME|\FilesystemIterator::CURRENT_AS_FILEINFO|\FilesystemIterator::SKIP_DOTS;
-        $iter = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($this->cachePath, flags: $flags));
-
         /** @var \SplFileInfo $file */
-        foreach ($iter as $file) {
-            unlink($file->getRealPath());
-        }
-    }
-
-    private function clearPregenerated(): void
-    {
-        print "Removing generated static files.\n";
-
-        $filter = static fn(\SplFileInfo $f) => $f->getBasename() !== 'index.php';
-
-        $flags = \FilesystemIterator::KEY_AS_PATHNAME|\FilesystemIterator::CURRENT_AS_FILEINFO|\FilesystemIterator::SKIP_DOTS;
-        $iter = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($this->publicPath, flags: $flags));
-        $files = new \CallbackFilterIterator($iter, $filter);
-
-        /** @var \SplFileInfo $file */
-        foreach ($files as $file) {
-            unlink($file->getRealPath());
+        foreach($files as $file) {
+            print "Removing {$file->getRealPath()}\n";
+            if ($file->isDir()){
+                rmdir($file->getPathname());
+            } else {
+                unlink($file->getPathname());
+            }
         }
     }
 }
