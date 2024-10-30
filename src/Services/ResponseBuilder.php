5828653620352d8488c5461e333ea847886aecbd
Larry Garfield <larry@garfieldtech.com>
2024-10-30 00:59:21 -0500

    Combine the various cache handling methods into one, to avoid double-calling hash or mtime functions.

--- src/Services/ResponseBuilder.php
+++ src/Services/ResponseBuilder.php
@@ -96,55 +96,38 @@ readonly class ResponseBuilder
     }
 
     /**
-     * Checks if a request that corresponds to a file should be treated as Not Modified.
+     * Wraps a request handler in HTTP cache handling, based on a specified file.
+     *
+     * This method only handles last-modified and ETag cache headers.  It does not
+     * set cache lifetimes.  A cache lifetime set by the $generator will be left untouched.
      *
      * @param ServerRequestInterface $request
      *   The incoming request.
      * @param string $filePath
-     *   The absolute (or stream) path to a file on disk.  This file will be checked against
-     *   both the if-modified-since header (based on the file's mtime) and if-none-match header
-     *   (aka ETag, based on a hash of the file).
-     * @return ResponseInterface|null
-     *   Either a 304 Not Modified response object, or null if the cache check failed.
-     */
-    public function handleFileCacheHeaders(ServerRequestInterface $request, string $filePath): ?ResponseInterface
-    {
-        $ifModifiedSince = $request->getHeaderLine('if-modified-since');
-        if ($ifModifiedSince && new \DateTimeImmutable($ifModifiedSince) >= new \DateTimeImmutable('@' . filemtime($filePath))) {
-            return $this->notModified();
-        }
-
-        $ifNoneMatch = $request->getHeaderLine('if-none-match');
-        if ($ifNoneMatch === hash_file(self::ETAG_HASH_ALGORITHM, $filePath)) {
-            return $this->notModified();
-        }
-
-        return null;
-    }
-
-    /**
-     * Adds cache headers to a response, based on a provided file.
-     *
-     * Both last-modified and etag headers will be added.
-     *
-     * @param ResponseInterface $response
-     * @param string $filePath
+     *   The absolute (or stream) path to a file on disk. The file's last-modified time will be used
+     *   for time-based comparison, and a hash of the file contents will be used for ETag comparison.
+     * @param \Closure $generator
+     *   The callable thunk that will generate the response if necessary.
      * @return ResponseInterface
+     *   Either a 304 Not Modified response, or a new response with appropriate cache headers.
      * @throws \Exception
      */
-    public function withFileCacheHeaders(ResponseInterface $response, string $filePath): ResponseInterface
-    {
-        return $response
-            ->withHeader('last-modified', (new \DateTimeImmutable('@' . filemtime($filePath)))->format('r'))
-            ->withHeader('etag', hash_file(self::ETAG_HASH_ALGORITHM, $filePath));
-    }
-
     public function handleCacheableFileRequest(ServerRequestInterface $request, string $filePath, \Closure $generator): ResponseInterface
     {
-        if ($cacheResponse = $this->handleFileCacheHeaders($request, $filePath)) {
-            return $cacheResponse;
+        $mtime = filemtime($filePath);
+        $etag = hash_file(self::ETAG_HASH_ALGORITHM, $filePath);
+
+        $ifModifiedSince = $request->getHeaderLine('if-modified-since');
+        if ($ifModifiedSince && new \DateTimeImmutable($ifModifiedSince) >= new \DateTimeImmutable('@' . $mtime)) {
+            return $this->notModified();
         }
 
-        return $this->withFileCacheHeaders($generator(), $filePath);
+        if ($request->getHeaderLine('if-none-match') === $etag) {
+            return $this->notModified();
+        }
+
+        return $generator()
+            ->withHeader('last-modified', (new \DateTimeImmutable('@' . $mtime))->format('r'))
+            ->withHeader('etag', $etag);
     }
 }
