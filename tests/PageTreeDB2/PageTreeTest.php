ae771d44b88bc8ea0da66ae095f6bf439440f2de
Larry Garfield <larry@garfieldtech.com>
2024-12-07 00:28:31 -0600

    Run filesystem-using tests in their own processes. It's slower than a static, but less buggy.

--- tests/PageTreeDB2/PageTreeTest.php
+++ tests/PageTreeDB2/PageTreeTest.php
@@ -4,25 +4,28 @@ declare(strict_types=1);
 
 namespace Crell\MiDy\PageTreeDB2;
 
-use Crell\MiDy\ClassFinder;
 use Crell\MiDy\Config\StaticRoutes;
 use Crell\MiDy\MarkdownDeserializer\MarkdownPageLoader;
 use org\bovigo\vfs\vfsStream;
 use org\bovigo\vfs\vfsStreamDirectory;
 use PHPUnit\Framework\Attributes\Before;
-use PHPUnit\Framework\Attributes\BeforeClass;
+use PHPUnit\Framework\Attributes\RunInSeparateProcess;
+use PHPUnit\Framework\Attributes\RunTestsInSeparateProcesses;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Tests that use the VFS need to run in their own separate processes.
+ *
+ * Otherwise, if a .php file is parsed, it will result in double-declaration
+ * of the class, and a fatal error.
+ */
+#[RunTestsInSeparateProcesses]
 class PageTreeTest extends TestCase
 {
     use SetupCache;
 
-    /**
-     * The VFS needs to be static so it's reused, so the require_once() call in the PHP interpreter
-     * can not require the "same" file multiple times, leading to double-declaration errors.
-     */
-    protected static vfsStreamDirectory $vfs;
+    protected vfsStreamDirectory $vfs;
 
     private Parser $parser;
 
@@ -38,8 +41,8 @@ class PageTreeTest extends TestCase
         $this->parser = new Parser($this->setupCache(), $fileParser);
     }
 
-    #[BeforeClass]
-    public static function initFilesystem(): vfsStreamDirectory
+    #[Before]
+    public function initFilesystem(): vfsStreamDirectory
     {
         // This mess is because vfsstream doesn't let you create multiple streams
         // at the same time.  Which is dumb.
@@ -48,13 +51,13 @@ class PageTreeTest extends TestCase
             'routes' => [],
         ];
 
-        return self::$vfs = vfsStream::setup('root', null, $structure);
+        return $this->vfs = vfsStream::setup('root', null, $structure);
     }
 
     #[Test]
     public function can_lazy_load_folder(): void
     {
-        $routesPath = self::$vfs->getChild('routes')?->url();
+        $routesPath = $this->vfs->getChild('routes')?->url();
 
         file_put_contents($routesPath . '/foo.html', 'Foo');
         file_put_contents($routesPath . '/bar.html', 'Bar');
@@ -71,7 +74,7 @@ class PageTreeTest extends TestCase
     #[Test]
     public function can_instantiate_pages(): void
     {
-        $routesPath = self::$vfs->getChild('routes')?->url();
+        $routesPath = $this->vfs->getChild('routes')?->url();
 
         file_put_contents($routesPath . '/single.html', 'Single');
         file_put_contents($routesPath . '/double.html', 'Double, HTML');
@@ -91,7 +94,7 @@ class PageTreeTest extends TestCase
     #[Test]
     public function can_instantiate_supported_file_types(): void
     {
-        $routesPath = self::$vfs->getChild('routes')?->url();
+        $routesPath = $this->vfs->getChild('routes')?->url();
 
         file_put_contents($routesPath . '/static.html', 'Foo');
         file_put_contents($routesPath . '/markdown.md', 'Bar');
