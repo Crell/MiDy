17140fa34a09689d65c2f61eeaa63b8db767abbb
Larry Garfield <larry@garfieldtech.com>
2024-12-23 23:35:47 -0600

    Port StaticFilePregenerator to use PageTree.

--- src/Commands/StaticFilePregenerator.php
+++ src/Commands/StaticFilePregenerator.php
@@ -5,17 +5,20 @@ declare(strict_types=1);
 namespace Crell\MiDy\Commands;
 
 use Crell\MiDy\Config\StaticRoutes;
-use Crell\MiDy\PageTree\Page;
-use Crell\MiDy\PageTree\PageFile;
-use Crell\MiDy\PageTree\RootFolder;
+use Crell\MiDy\PageTreeDB2\PageCacheDB;
+use Crell\MiDy\PageTreeDB2\PageFile;
+use Crell\MiDy\PageTreeDB2\PageTree;
 use DI\Attribute\Inject;
 
+use function Crell\fp\itmap;
+use function Crell\fp\pipe;
 use function Crell\MiDy\ensure_dir;
 
 readonly class StaticFilePregenerator
 {
     public function __construct(
-        private RootFolder $root,
+        private PageTree $tree,
+        private PageCacheDB $cache,
         private StaticRoutes $staticRoutes,
         #[Inject('paths.public')]
         private string $publicPath,
@@ -23,22 +26,21 @@ readonly class StaticFilePregenerator
 
     public function run(): void
     {
-        $files = new \CallbackFilterIterator($this->pageFileIterator(), $this->filterStatic(...));
+        // First, ensure the index is fully up to date.
+        $this->tree->reindexAll();
 
-        /** @var PageFile $file */
-        foreach ($files as $file) {
-            $dest = $this->publicPath . $file->logicalPath . '.' . $file->ext;
-            ensure_dir(pathinfo($dest, PATHINFO_DIRNAME));
-            copy($file->physicalPath, $dest);
-        }
+        // Now get every single page in the index, and copy it to the target path.
+        pipe($this->cache->allFiles(),
+            itmap($this->filterStatic(...)),
+            itmap($this->copyFile(...))
+        );
     }
 
-    private function pageFileIterator(): \Generator
+    private function copyFile(PageFile $file): void
     {
-        /** @var Page $page */
-        foreach ($this->root->descendants(false) as $page) {
-            yield from $page->variants();
-        }
+        $dest = $this->publicPath . $file->path . '.' . $file->ext;
+        ensure_dir(pathinfo($dest, PATHINFO_DIRNAME));
+        copy($file->physicalPath, $dest);
     }
 
     private function filterStatic(PageFile $p): bool
