e29a08c346515e3d389e595bf26aeaf7444713de
Larry Garfield <larry@garfieldtech.com>
2025-01-29 23:19:30 -0600

    Fold Hidable into the PageInformation interface, as it's never used separately.

--- src/PageTree/PageInformation.php
+++ src/PageTree/PageInformation.php
@@ -4,12 +4,13 @@ declare(strict_types=1);
 
 namespace Crell\MiDy\PageTree;
 
-interface PageInformation extends Hidable
+interface PageInformation
 {
     public string $title { get; }
     public string $summary { get; }
     public array $tags { get; }
     public ?string $slug { get; }
+    public bool $hidden { get; }
 
     public function hasAnyTag(string ...$tags): bool;
     public function hasAllTags(string ...$tags): bool;
