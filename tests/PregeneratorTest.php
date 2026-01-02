4446c4f029eb562174c3be5ba573e38f25bef0ce
Larry Garfield <larry@garfieldtech.com>
2026-01-02 13:59:14 -0600

    Remove deprecated attribute.

--- tests/PregeneratorTest.php
+++ tests/PregeneratorTest.php
@@ -6,15 +6,15 @@ namespace Crell\MiDy;
 
 use Crell\MiDy\Commands\StaticFilePregenerator;
 use PHPUnit\Framework\Attributes\DoesNotPerformAssertions;
-use PHPUnit\Framework\Attributes\RunClassInSeparateProcess;
+use PHPUnit\Framework\Attributes\RunInSeparateProcess;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
 
 // This is a total hack for now, so I can debug things.
-#[RunClassInSeparateProcess]
 class PregeneratorTest extends TestCase
 {
     #[Test, DoesNotPerformAssertions]
+    #[RunInSeparateProcess]
     public function test(): void
     {
         $app = new MiDy('.', routesPath: \realpath('tests/test-routes'), publicPath: 'tests/test-public');
